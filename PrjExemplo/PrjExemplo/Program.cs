66452f5b9a528c18212a8c6ae38f13b8df470459
BizarriaP <pedro.bizarria@aluno.ifsp.edu.br>
2026-03-26 22:13:45 -0300

    Add files via upload

---
+++ PrjExemplo/PrjExemplo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrjExemplo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(74);
+            Console.WriteLine(7.4);
+            Console.WriteLine("IFSP");
+            Console.WriteLine('*');
+
+            int timmaia;
+            int robertocarlos;
+            int beatles;
+            Console.Write("Digite o primeiro numero:");
+            timmaia = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo nuemero:");
+            robertocarlos = int.Parse(Console.ReadLine());
+
+            beatles = timmaia + robertocarlos;
+
+            Console.Write("Resultado da soma:");
+            Console.WriteLine(beatles);
+
+            Console.Write("Resultado da soma {0} com {1} é {2}", timmaia, robertocarlos, beatles);
+        }
+    }
+}
