bea64aad7f5e9da1eef84cd4f2b2b08c7b7aecf3
BizarriaP <pedro.bizarria@aluno.ifsp.edu.br>
2026-05-14 21:49:26 -0300

    Add files via upload

---
+++ Lista1Ex1/ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+
+            Console.WriteLine("qual a altura");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("qual a base");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("A área do retangulo é " + a * b);
+        }
+    }
+}
---
+++ Lista1Ex2/Lista1Ex2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double lado;
+            double area;
+
+            Console.WriteLine("informe o lado do quadrado: ");
+            lado = double.Parse(Console.ReadLine());
+            area = lado * lado;
+
+            Console.WriteLine("A área do quadrado é: ");
+            Console.WriteLine(area);
+        }
+    }
+}
