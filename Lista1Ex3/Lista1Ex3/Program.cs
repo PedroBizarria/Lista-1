969c1ae49f11d70d9da2c785a98a46fd8775b857
BizarriaP <pedro.bizarria@aluno.ifsp.edu.br>
2026-05-14 22:03:46 -0300

    Add files via upload

---
+++ Lista1Ex3/Lista1Ex3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Diagonal;
+            double area;
+
+            Console.WriteLine("informe a diagonal do quadrado: ");
+            Diagonal = double.Parse(Console.ReadLine());
+
+            area = (Diagonal * Diagonal) / 2;
+            
+            Console.WriteLine("A area do quadrado é: ");
+            Console.WriteLine(area);
+        }
+    }
+}
