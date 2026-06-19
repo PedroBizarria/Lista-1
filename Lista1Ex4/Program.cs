42ac026b6c773b0bf3675e78c3e6603d283de554
BizarriaP <pedro.bizarria@aluno.ifsp.edu.br>
2026-06-18 21:19:54 -0300

    Add files via upload

---
+++ Lista1Ex4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double b;
+            double a;
+
+            Console.WriteLine("Digite o valor da base do triângulo: ");
+            b = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Digite o valor da altura do triângulo: ");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("A área do triângulo é {0}", (b * a) / 2);
+        }
+    }
+}
